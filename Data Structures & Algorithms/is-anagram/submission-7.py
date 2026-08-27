d68f6b23ea0536d3db2d8983af72cb1fef5862cd
Piyush <piyush.gag6513@gmail.com>
2026-08-27 08:27:41 +0530

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashone = {}
+        hashtwo = {}
+
+        if len(s) != len(t):
+            return False
+        for i in s:
+            hashone[i] = hashone.get(i, 0) + 1
+        for i in t:
+            hashtwo[i] = hashtwo.get(i, 0) + 1
+            
+        if hashone == hashtwo:
+            return True
+        else:
+            return False
---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i = 0;
+        vector<int> ans;
+        while(i < n){
+            for (int j = 0; j < n; j++){
+                
+               
+                if(nums[i] + nums[j] == target){
+                    if(i == j){
+                    continue;
+                }
+                 if(nums[i] + nums[j] > target){
+                    continue;
+                }
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+
+            }
+            i++;
+        }
+        
+    }
+};
