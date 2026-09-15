208302172b4ea227d558c08f7a4d321a73592a36
Piyush <piyush.gag6513@gmail.com>
2026-09-15 16:21:13 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = collections.deque()
+        length = len(s)
+
+        if length == 0 or length%2!=0:
+            return False
+        if s[0] == ']' or s[0] == '}' or s[0] == ')':
+            return False
+        for c in s:
+            if c == '{' or c == '[' or c == '(':
+                stack.append(c)
+        # for c in s:
+            if len(stack) == 0 and c in [']', ')', '}']:
+                return False
+            # if c in [']', ')', '}'] and stack[-1] in ['[', '(', '{']:
+            #     return False
+            if len(stack) != 0 and c in [']', ')', '}']:
+                if c == ']' and stack[-1] == '[':
+                    stack.pop()
+                elif c == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif c == ')' and stack[-1] == '(':
+                    stack.pop()
+                else: return False
+            # else: return False
+        if stack:
+            return False
+        else:
+            return True
+        
